bab6b36028cb4d75c56263a7f830125e1ffac054
Michel Frechette <mifrec@hotmail.com>
2016-03-27 07:06:59 -0400

    Doing Math

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-// var passphrase = 'Open Sesame';
-// console.log(passphrase.length);
-// console.log(passphrase.toLowerCase());
-// console.log(passphrase);
-// console.log(passphrase.toUpperCase());
-//
-// var stringToShout = prompt("What should I shout?")
-// var shout = stringToShout.toUpperCase();
-// shout += "!!!";
-// alert(shout);
+var secondsPerMinute = 60;
+var minutesPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+
+var secondsPerDay = secondsPerMinute * minutesPerHour * hoursPerDay;
+document.write("There are " + secondsPerDay + " seconds in a day");
