6e28bb215426b6f72485eaf4a60a08c77561125b
Michel Frechette <mifrec@hotmail.com>
2016-03-27 09:37:57 -0400

    Numbers and Strings

--- numbers.js
+++ numbers.js
@@ -3,3 +3,8 @@ var numberOfTeles = prompt("Number of Teles");
 var numberOfLesPauls = prompt("Number of Les Pauls");
 var totalGuitars = parseInt(numberOfStrats) + parseFloat(numberOfTeles) + parseInt(numberOfLesPauls);
 document.write("<h2> Total number of guitars: " + totalGuitars)
+
+var width = '190px';
+var numOfDivs = 10;
+var totalWidth = parseInt(width) * parseInt(numOfDivs);
+document.write("<h2> Total width: " + totalWidth + "px.</h2>");
