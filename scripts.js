a3f7d8f08c5a0b5f84f695394c2cb66f6727b21a
Michel Frechette <mifrec@hotmail.com>
2016-03-27 03:10:48 -0400

    Working with Strings and Finding Help

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-var visitor = prompt("What is your name?");
-var message = 'Hello ' + visitor + ". Welcome to Treehouse.";
-message += " We are so glad that you came by to visit.";
-message += visitor;
-message += ". Please come again, when you want to learn some more.";
-document.write(message);
+var passphrase = 'Open Sesame';
+console.log(passphrase.length);
+console.log(passphrase.toLowerCase());
+console.log(passphrase);
+console.log(passphrase.toUpperCase());
+
+var stringToShout = prompt("What should I shout?")
+var shout = stringToShout.toUpperCase();
+shout += "!!!";
+alert(shout);
