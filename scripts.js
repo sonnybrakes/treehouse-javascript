7b4fb4c6f2d57cdbc593a1246dc17b992eea355a
Michel Frechette <mifrec@hotmail.com>
2016-03-28 06:56:19 -0400

    Improving the Random Number Guessing Game

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ var randomNumber = Math.floor(Math.random() * 6 ) + 1;
 var guess = prompt('I am thinking of a number between 1 and 6. What is it');
 if (parseInt(guess) === randomNumber) {
   correctGuess = true;
+} else if ( parseInt(guess) < randomNumber ) {
+  var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess);
+  if (parseInt(guessMore) === randomNumber) {
+    correctGuess = true;
+  }
+} else if ( parseInt(guess) > randomNumber ) {
+  var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess);
+  if (parseInt(guessLess) === randomNumber) {
+    correctGuess = true; 
+  }
 }
 if (correctGuess===true) {
   document.write('<p>You guessed the number!</p>');
