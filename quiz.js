396bb409f8db37ad7b1f9a66651e1a66c3207a52
Michel Frechette <mifrec@hotmail.com>
2016-03-28 05:58:10 -0400

    Introducing Conditional Statements

---
+++ quiz.js
@@ -0,0 +1,7 @@
+var answer = prompt('What programming is the name of a gem?');
+
+if ( answer.toUpperCase() === 'RUBY') {
+  document.write("<p>That's right!</p>");
+} else {
+  document.write("<p>Sorry, that's wrong.</p>");
+}
