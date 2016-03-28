c764c44acbc932fb77e1c2599503d84ad5196c24
Michel Frechette <mifrec@hotmail.com>
2016-03-28 06:23:49 -0400

    Build a Random Number Guessing Game

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-var secondsPerMinute = 60;
-var minutesPerHour = 60;
-var hoursPerDay = 24;
-var daysPerWeek = 7;
-var weeksPerYear = 52;
+// var secondsPerMinute = 60;
+// var minutesPerHour = 60;
+// var hoursPerDay = 24;
+// var daysPerWeek = 7;
+// var weeksPerYear = 52;
+//
+// var secondsPerDay = secondsPerMinute * minutesPerHour * hoursPerDay;
+// document.write("There are " + secondsPerDay + " seconds in a day");
 
-var secondsPerDay = secondsPerMinute * minutesPerHour * hoursPerDay;
-document.write("There are " + secondsPerDay + " seconds in a day");
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+var guess = prompt('I am thinking of a number between 1 and 6. What is it');
+if (parseInt(guess) === randomNumber) {
+  document.write('<p>You guessed the number!</p>');
+} else {
+  document.write('<p>Sorry. The number was ' + randomNumber + '</p>');
+}
