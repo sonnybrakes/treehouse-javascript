35ae815a0f174fd4979eff758608878d9543541c
Michel Frechette <mifrec@hotmail.com>
2016-03-28 19:00:58 -0400

    Giving Information to Functions

--- random.js
+++ random.js
@@ -1,5 +1,17 @@
-function alertRandom() {
-  var randomNumber = Math.floor( Math.random() * 6 ) + 1;
-  alert(randomNumber);
+// function getRandomNumber( upper) {
+//   var randomNumber = Math.floor( Math.random() * upper ) + 1;
+//   document.write(randomNumber);
+//   return randomNumber;
+// }
+// console.log( getRandomNumber(49) );
+// console.log( getRandomNumber(49) );
+
+// var dieRoll = getRandomNumber();
+// console.log( getRandomNumber() );
+// alert( getRandomNumber() );
+
+function getArea(width, length, unit) {
+  var area = width * length;
+  return area + " " + unit;
 }
-alertRandom();
+console.log(getArea(15, 20, 'sq ft'));
