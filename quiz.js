b09b7e49842a9048460fbcdf75562695d82b8f06
Michel Frechette <mifrec@hotmail.com>
2016-03-28 07:44:03 -0400

    The Conditional Challenge Solution

--- quiz.js
+++ quiz.js
@@ -1,7 +1,38 @@
-var answer = prompt('What programming is the name of a gem?');
+// quiz begins, no answer correct
+var correct = 0;
 
-if ( answer.toUpperCase() === 'RUBY') {
-  document.write("<p>That's right!</p>");
+// ask questions
+var answer1 = prompt("Name a programming languages that's also a gem");
+if (answer1.toUpperCase() === 'Ruby') {
+  correct += 1;
+}
+var answer2 = prompt("Name a programming language that's also a snake");
+if (answer2.toUpperCase() === 'Python') {
+  correct += 1;
+}
+var answer3 = prompt("What language do you use to style web pages?");
+if (answer3.toUpperCase() === 'css') {
+  correct += 1;
+}
+var answer4 = prompt("What language do you use to build the structure of a web page?");
+if (answer4.toUpperCase() === 'html') {
+  correct += 1;
+}
+var answer5 = prompt("What language do you use to add interactivity to a web page?");
+if (answer5.toUpperCase() === 'javascript') {
+  correct += 1;
+}
+
+// output results
+document.write("<p>You got " + correct + " out of 5 questions correct.</p>");
+
+// output rank
+if (correct === 5) {
+  document.write("<p><strong>You earned a gold crown!</strong></p>");
+} else if (correct >= 3) {
+  document.write("<p><strong>You earned a silver crown!</strong></p>");
+} else if (correct >= 1) {
+  document.write("<p><strong>You earned a bronze crown!</strong></p>");
 } else {
-  document.write("<p>Sorry, that's wrong.</p>");
+  document.write("<p><strong>No crown for you. You need to study!</strong></p>");
 }
